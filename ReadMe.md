df8ae50c6697014a5eee9fc72e883b8550210967
panda141035 <panda141035@gmail.com>
2022-04-17 10:29:00 +0300

    Last Release

--- ReadMe.md
+++
@@ -1,1 +0,0 @@
-updated at Fri Apr 16 2021 09:20:00 
---
+++ frontend/src/config.js
@@ -0,0 +1,6 @@
+export const config = {
+  chainId: 56,
+  contractAddress: "0x630a827e0784ab4ff2c1b8aC13b8139d8BFEE2e0",
+  scanLink:
+    "https://www.bscscan.com/address/0x630a827e0784ab4ff2c1b8aC13b8139d8BFEE2e0"
+};
